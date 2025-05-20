7ca0b268d8e10c942f8dace87afdca3b532c71ef
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2025-05-20 16:35:28 +0530

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for Render
-  }
 });
 
-export default pool;
